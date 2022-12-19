149a1ac4784fbe625c6141e0e25342c8045d6831
Dharmendra Yadav <you@example.com>
2022-12-19 18:53:37 +0530

    Final Code

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes/routes");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+//*****************************[MongoDB-Connected]********************************//
+mongoose
+  .connect(
+    "mongodb+srv://Dharmendra:dkyadav123@cluster0.kq9bu.mongodb.net/myLoginRegisterDB",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then((result) => console.log("Hello Mr Dkyadav MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+//********************************[Port Created]**********************************//
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
+});
